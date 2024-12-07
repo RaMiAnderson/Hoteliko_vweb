8c2f25a2504725b3c3bd1b03ad5ac871006c5786
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-07 13:38:53 +0100

    mlml

--- client/src/components/inputDate/DatePicker.jsx
+++ client/src/components/inputDate/DatePicker.jsx
@@ -4,10 +4,11 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { useTheme } from '../../context/themeContext';
 
 export default function ControlledComponent({label}) {
   const [value, setValue] = React.useState(null);
-
+  const {theme} = useTheme()
   return (
     <LocalizationProvider  dateAdapter={AdapterDayjs}>
       <DemoContainer  components={['DatePicker']}>
@@ -17,8 +18,25 @@ export default function ControlledComponent({label}) {
           value={value} 
           onChange={(newValue) => setValue(newValue)}
           sx={{
-            width: '30px !important',  // Définir la largeur
-          }}/>
+            width: '30px !important', // Ajuster la largeur de l'input
+            "& .MuiInputLabel-root": {
+              color: theme === "light" ? "black" : "white", 
+            },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: theme === "light" ? "black" : "white", 
+              },
+              "&:hover fieldset": {
+                borderColor: theme === "light" ? "black" : "white", 
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: theme === "light" ? "black" : "white", 
+              },
+            },
+          }}
+          
+          
+          />
       </DemoContainer>
     </LocalizationProvider>
   );
