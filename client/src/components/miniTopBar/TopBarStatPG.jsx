6c021620fbfda7f5d1bec50c6791a8fd0a1fa69a
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-12-07 13:28:05 +0100

    op

--- client/src/components/miniTopBar/TopBarStatPG.jsx
+++ client/src/components/miniTopBar/TopBarStatPG.jsx
@@ -13,7 +13,7 @@ import { useTheme } from '../../context/themeContext';
 export default function TopBarStatPG({titlePg}) {
   const {theme} = useTheme()
   return (
-    <div className='mnTpBr_Parent' style={theme==="light" ? {backgroundColor:"white"} :{backgroundColor:"black"} }>
+    <div className='mnTpBr_Parent' style={theme==="light" ? {backgroundColor:"white"} :{backgroundColor:"rgba(51, 51, 51, 0.966)"} }>
         <h3 className='titlePgStyle'>{titlePg}</h3> 
         <div className='mnTpBr_rightPrt'>
             <div className='BtnNewTick'>
