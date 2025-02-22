5b77b7753f6c1523d8929e4633011994b630a4a6
Mickaël Anderson <123479860+RaMiAnderson@users.noreply.github.com>
2025-02-22 19:49:14 +0300

    card Money

--- client/src/pages/content/dashBoard/dashBoardContent.jsx
+++ client/src/pages/content/dashBoard/dashBoardContent.jsx
@@ -12,13 +12,14 @@ const dashBoardContent = () => {
          <div className="partOne">
           <CardMoney color="var(--cardMVente)" label="Total Vente" montant="15 000 000" stats="+10%"/>
           <CardMoney color="var(--cardMRevient)" label="Total Revient" montant="11 000 000"/>
-          <CardMoney color="var(--cardMRevient)" label="Total Revient" montant="11 000 000"/>
+          <CardMoney color="var(--cardMDepense)" label="Total Dépense" montant="1 000 000"/>
          </div>
          <div className="partOne">
-          <CardMoney color="var(--cardMVente)" label="Total Vente" montant="15 000 000" stats="+10%"/>
-          <CardMoney color="var(--cardMRevient)" label="Total Revient" montant="11 000 000"/>
-          <CardMoney color="var(--cardMRevient)" label="Total Revient" montant="11 000 000"/>
+          <CardMoney color="var(--cardMVente)" label="Total Encaisse" montant="13 000 000" stats="+10%"/>
+          <CardMoney color="var(--cardMVente)" label="Reste Encaisse" montant="2 000 000"/>
+          <CardMoney color="var(--cardMEncaisse)" label="Total Bénéfice" montant="3 000 000"/>
          </div>
+         
         </div>
         {/* bottomPart */}
         <div className="bottomPart">
